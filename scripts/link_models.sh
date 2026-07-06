72a38f3e55b6a785841fe3016ed56aaf4008520e
Yen-Chieh Huang <lashhw81@gmail.com>
2026-07-06 20:56:04 +0800

    add fig7 actions

--- scripts/link_models.sh
+++ scripts/link_models.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 for model in shufflenet mobilenet inception; do
     ln -s /4TB/aeuser/DupNAS-AE/DupNAS/sample_onnx/$model/*.onnx DupNAS/sample_onnx/$model/
 done
+
+ln -s /4TB/aeuser/DupNAS-AE/DupNAS/NASBase/dataset DupNAS/NASBase/
